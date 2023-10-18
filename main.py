eea7f0b8df1c16a5eda6d68e47760c3e67197f20
ivanleopoldo <ivanleopoldoc@gmail.com>
2023-10-18 19:06:02 +0800

    v1

--- main.py
+++ main.py
@@ -1,65 +1,57 @@
-# references
-#       https://github.com/bgrohman/Brute-Force-Sudoku-Solver/blob/master/SudokuSolver.py
-
-
 import random
 
 # global vars
 N = 5
 
 
-def init_board():
-    """initializes 5x5 board
-
-    Returns:
-        list: a 5x5 matrix with initial values "x"
-    """
-    return [["x" for _ in range(N)] for _ in range(N)]
-
-
 def display_board(board):
-    """displays 5x5 board
-
-    Args:
-        board list: 5x5 matrix
-    """
     print("   ", " ".join(map(str, [n for n in range(N)])))
     print("   ", " ".join(map(str, ["-" for _ in range(N)])))
     for idx, row in enumerate(board):
         print(f"{idx} |", " ".join(map(str, row)))
 
 
-def find_empty(board):
-    """finds next empty cell in board
-
-    Args:
-        board list: 5x5 matrix
-
-    Returns:
-        tuple: cell coordinates
-    """
-    for i in range(N):
-        for j in range(N):
-            if board[i][j] == "x":
-                return (i, j)
+def valid(board, cell, guess):
+    return (False if guess in board[cell[0]] else True) and all(
+        board[i][cell[1]] != guess for i in range(N)
+    )
 
 
-def valid_for_row(board, cell):
-    return True
+def solve(board, cell):
+    # check if function reached end of board
+    if cell[0] == N - 1 and cell[1] == N:
+        return True
 
+    # check if column cell coords reached end of row
+    # then move to next row
+    if cell[1] == N:
+        cell[0], cell[1] = cell[0] + 1, 0
 
-def valid_for_col(board, cell):
-    return True
+    # check if specific cell already contains value
+    # move to the next column
+    if board[cell[0]][cell[1]] > 0:
+        return solve(board, [cell[0], cell[1] + 1])
 
+    # inputs guesses
+    for num in range(1, N + 1):
+        # if guess is valid change cell value to guess
+        if valid(board, cell, num):
+            board[cell[0]][cell[1]] = num
 
-def valid(board, cell):
-    return valid_for_row(board, cell) and valid_for_col(board, cell)
+            # guess next column
+            if solve(board, [cell[0], cell[1] + 1]):
+                return True
+
+        # if guess not valid change value to 0
+        board[cell[0]][cell[1]] = 0
+
+    return False
 
 
 def main():
-    numbers = [1, 2, 3, 4, 5]
-    random.shuffle(numbers)
-    board = init_board()
+    # initialize
+    numbers = random.sample(range(1, N + 1), N)
+    board = [[0] * N for _ in range(N)]
     display_board(board)
 
     user_list = []
@@ -74,7 +66,11 @@ def main():
         board[i[0]][i[1]] = j
 
     display_board(board)
-    valid_for_row(board, [0, 1])
+    print("\n")
+    if solve(board, [0, 0]):
+        display_board(board)
+    else:
+        print("no solution")
 
 
 if __name__ == "__main__":
